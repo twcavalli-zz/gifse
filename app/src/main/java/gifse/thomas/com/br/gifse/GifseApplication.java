c8cee2444b2f7ede69c2a9e22cd7105e14021ae5
twcavalli <twcavalli@@users.noreply.github.com>
2017-07-11 20:27:53 -0300

    Add libraries and real config file

--- app/src/main/java/gifse/thomas/com/br/gifse/GifseApplication.java
+++ app/src/main/java/gifse/thomas/com/br/gifse/GifseApplication.java
@@ -1,8 +1,20 @@
 package gifse.thomas.com.br.gifse;
 
+import android.app.Application;
+
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+
 /**
  * Created by Thomas on 11/07/2017.
  */
 
-public class GifseApplication {
+public class GifseApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Realm.init(this);
+        RealmConfiguration realmConfiguration = new RealmConfiguration.Builder().build();
+        Realm.setDefaultConfiguration(realmConfiguration);
+    }
 }
