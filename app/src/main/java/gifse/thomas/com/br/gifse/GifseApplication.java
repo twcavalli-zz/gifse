9bfa1c41d5ab1fc14c92462b6b396838b78c3622
twcavalli <twcavalli@@users.noreply.github.com>
2017-07-11 20:20:55 -0300

    Create project

---
+++ app/src/main/java/gifse/thomas/com/br/gifse/GifseApplication.java
@@ -0,0 +1,8 @@
+package gifse.thomas.com.br.gifse;
+
+/**
+ * Created by Thomas on 11/07/2017.
+ */
+
+public class GifseApplication {
+}
---
+++ app/src/main/java/gifse/thomas/com/br/gifse/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package gifse.thomas.com.br.gifse.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import gifse.thomas.com.br.gifse.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
